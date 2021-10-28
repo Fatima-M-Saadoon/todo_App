b72d59dc3d24689df536394a153d1e5654287144
FlutLab.io <>
2021-10-28 20:51:22 +0000

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:todo_app1/main.dart' as app;
+import 'package:todo_app_embbedv2/main.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+  app.main();
+}
